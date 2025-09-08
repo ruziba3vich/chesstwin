42bd59c23787fd5eb72a99264b0e1c54ebfdbdc5
ruziba3vich <rruzibayevich@gmail.com>
2025-09-09 01:13:35 +0500

    feat: create login request

---
+++ src/main/java/com/prodonik/chesstwin/dto/UserLoginRequest.java
@@ -0,0 +1,12 @@
+package com.prodonik.chesstwin.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import lombok.Data;
+
+@Data
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class UserLoginRequest {
+    private String username;
+}
