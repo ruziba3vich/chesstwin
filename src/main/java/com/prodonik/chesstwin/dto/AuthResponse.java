c53b09dd91a8d08c44824c36bdd68992e00e0a95
ruziba3vich <rruzibayevich@gmail.com>
2025-09-09 00:11:41 +0500

    fix: remove builder

--- src/main/java/com/prodonik/chesstwin/dto/AuthResponse.java
+++ src/main/java/com/prodonik/chesstwin/dto/AuthResponse.java
@@ -1,10 +1,10 @@
 package com.prodonik.chesstwin.dto;
 
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@Builder
+@AllArgsConstructor
 public class AuthResponse {
     private String accessToken;
     private String refreshToken;
