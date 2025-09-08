319848da72a6009475a9e2caae928badc4ceb5f7
ruziba3vich <rruzibayevich@gmail.com>
2025-09-08 19:16:43 +0500

    fix: use lombok[Data] annotation

--- src/main/java/com/prodonik/chesstwin/dto/UserCreateRequest.java
+++ src/main/java/com/prodonik/chesstwin/dto/UserCreateRequest.java
@@ -1,12 +1,13 @@
 package com.prodonik.chesstwin.dto;
 
+import lombok.Data;
+
+@Data
 public class UserCreateRequest {
     private String fullname;
     private String username;
-
-    public String getFullname() { return fullname; }
-    public void setFullname(String fullname) { this.fullname = fullname; }
-
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
+    private int avgOpeningElo;
+    private int avgMidgameElo;
+    private int avgEndgameElo;
+    private int gamesCount;
 }
--- src/main/java/com/prodonik/chesstwin/dto/UserDto.java
+++ src/main/java/com/prodonik/chesstwin/dto/UserDto.java
@@ -2,17 +2,17 @@ package com.prodonik.chesstwin.dto;
 
 import java.util.UUID;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class UserDto {
     private UUID id;
     private String fullname;
     private String username;
-
-    public UUID getId() { return id; }
-    public void setId(UUID id) { this.id = id; }
-
-    public String getFullname() { return fullname; }
-    public void setFullname(String fullname) { this.fullname = fullname; }
-
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
+    private int avgOpeningElo;
+    private int avgMidgameElo;
+    private int avgEndgameElo;
+    private int gamesCount;
 }
