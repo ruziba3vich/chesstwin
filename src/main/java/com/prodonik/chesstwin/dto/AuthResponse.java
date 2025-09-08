ed87f4f9eb6cb30764379b10a3f840b68786a494
ruziba3vich <rruzibayevich@gmail.com>
2025-09-09 00:08:22 +0500

    feat: create auth response data transfer object

---
+++ src/main/java/com/prodonik/chesstwin/dto/AuthResponse.java
@@ -0,0 +1,11 @@
+package com.prodonik.chesstwin.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class AuthResponse {
+    private String accessToken;
+    private String refreshToken;
+}
